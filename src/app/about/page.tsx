fe19c60b0da86cd3771406b43512f09530c03148
PawanK06 <pawan06kumar2003@gmail.com>
2024-04-06 23:33:49 +0530

    added aboute page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-w-4xl md:max-w-[800px] px-3 xl:p-0 mx-auto'>
+      About
+    </div>
+  )
+}
+
+export default page
