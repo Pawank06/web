52db301e1e634c32cf5af8704dd44953a6524737
PawanK06 <pawan06kumar2003@gmail.com>
2024-04-21 12:59:55 +0530

    worked on blog pavge

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,24 @@ import React from 'react'
 const page = () => {
   return (
     <Container>
-      About
+      <h1 className='font-semibold text-3xl'>About</h1>
+      <div className='pt-8 text-lg'>
+
+        <p>
+          Hi there! I&apos;m Pawan, and I love making things as a full-stack developer. I believe that the best way to learn is by doing and sharing, so I&apos;m always posting my progress and what I learn on social media.
+        </p>
+
+        <br />
+
+        <p className=''>
+          Right now, I&apos;m really into Solana and Web3, which are all about cool new ways of using the internet. I&apos;m learning all about them, and I&apos;ll be writing about my adventures on my blog.
+        
+
+        <br />
+
+        Come along with me on this journey of discovery! We&apos;ll figure out Solana, explore Web3, and maybe even build some cool stuff together. Let&apos;s learn and grow together in the world of tech, step by step.
+        </p>
+      </div>
     </Container>
   )
 }
